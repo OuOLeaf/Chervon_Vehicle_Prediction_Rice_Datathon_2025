2a648bb44d232abb71cfff440f3f62fb6b3a2e4a
chanzzes2366 <chandelier0207@gmail.com>
2025-02-01 23:31:10 -0800

    P3

--- streamlit.py
+++ streamlit.py
@@ -117,5 +117,5 @@ st.table(df)  #
 st.markdown("---")
 st.subheader("Model Performance & Interpretation")
 st.subheader("XGBoost Model : RMSE = 6043.28")
-st.image("Feature_Imp.png", width = 700)
+st.image("Feature_imp.png", width = 700)
 
