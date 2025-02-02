1a0c1c3571595a40eabbed08937a21f3c1cfa716
chanzzes2366 <chandelier0207@gmail.com>
2025-02-01 23:26:29 -0800

    P1

--- streamlit.py
+++ streamlit.py
@@ -6,10 +6,116 @@ import seaborn as sns
 
 
 st.title("Chervon: Vehicle Population Prediction")
-# st.set_page_config(layout="wide")
-st.image("cover.png", use_column_width=True)
+st.set_page_config(layout="wide")
+st.image("cover.png", use_container_width=True)
+
+data = {
+    "Column": [
+        "Date",
+        "Vehicle Category",
+        "GVWR Class",
+        "Fuel Type",
+        "Model Year",
+        "Fuel Technology",
+        "Electric Mile Range",
+        "Number of Vehicles Registered at the Same Address",
+        "Region",
+        "Vehicle Population",
+    ],
+}
+
+# Create a DataFrame
+df = pd.DataFrame(data)
+df.index = range(1, len(df) + 1)
 
 
-st.image("Vehicle_Age.png", use_column_width=True)
+# Display the table in Streamlit
+st.markdown("---")
+st.title("Column Information")
+st.table(df)  # Use st.dataframe(df) if you want interactivity
+data = {
+    "Variable": [
+        "Electric Mile Range",
+        "GVWR Class",
+        "Model Year",
+        "Fuel Type",
+        "Number of Vehicles Registered at the Same Address",
+        "Date",
+        "Vehicle Category",
+        "Fuel Technology",
+        "Region",
+        "Vehicle Population",
+    ],
+    "Missing Percentage (%)": [
+        97.413100,
+        55.937447,
+        1.468833,
+        0.209485,
+        0.080384,
+        0.000000,
+        0.000000,
+        0.000000,
+        0.000000,
+        0.000000,
+    ],
+}
 
-st.image("Vehicle_Age.png", use_column_width=True)
+# Create a DataFrame
+df = pd.DataFrame(data)
+df.index = range(1, len(df) + 1)
+
+st.markdown("---")
+# Display the table in Streamlit
+st.title("Missing Data Percentage Overview")
+st.table(df)  
+
+
+st.markdown("---")
+st.subheader("Feature Engineering")
+st.markdown("""
+### Vehicle Age in the Model
+
+- **Initially, we included Date and Model Year in our model as numeric columns, but the model's performance did not improve significantly.**
+- **Defined Vehicle Age as Date minus Model Year.**
+- **For rows where Model Year is missing, we fill the missing value with the median in train data.**
+- **Greatly enhanced the model's performance.**
+""")
+st.image("Vehicle_Age.png", width = 700)
+
+# Data for the table
+data = {
+    "Variables": [
+        "Vehicle Age",
+        "Vehicle Category",
+        "GVWR Class",
+        "Fuel Type",
+        "Fuel Technology",
+        "Electric Mile Range",
+        "Number of Vehicles Registered at the Same Address",
+        "Vehicle Population",
+    ],
+    "Description": [
+        "Date - Model Year",
+        "Nominal Variable",
+        "Nominal Variable",
+        "Nominal Variable",
+        "Nominal Variable",
+        "Nominal Variables, Non-applicable = 1, Miles Related Rows = 2, Unknown = 3",
+        "Order Variable",
+        "Response Variable",
+    ],
+}
+
+# Create a DataFrame
+df = pd.DataFrame(data)
+df.index = range(1, len(df) + 1)
+
+# Display the table in Streamlit
+st.subheader("Model Variable Explanation")
+st.table(df)  #
+
+st.markdown("---")
+st.subheader("Model Performance & Interpretation")
+st.subheader("XGBoost Model : RMSE = 6043.28")
+st.image("Feature_Imp.png", width = 700)
+
