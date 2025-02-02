4bdbd03ce8bfe84971869f3926ff658429282dbc
chanzzes2366 <chandelier0207@gmail.com>
2025-02-01 21:45:11 -0800

    Initial commit

---
+++ streamlit.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import xgboost as xgb
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Title of the dashboard
+st.title("Chervon: Vehicle Population Prediction")
+
+
+st.title("Average Vehicle Population by Year Difference Group")
+
