c1b6c7536ab2b982798bbd22781fde8cb07544b8
chanzzes2366 <chandelier0207@gmail.com>
2025-02-02 00:08:03 -0800

    P8

--- streamlit.py
+++ streamlit.py
@@ -7,32 +7,8 @@ import seaborn as sns
 
 st.title("Chervon: Vehicle Population Prediction")
 
-st.image("cover.png", use_container_width=True)
+#st.image("cover.png", use_container_width=True)
 
-data = {
-    "Column": [
-        "Date",
-        "Vehicle Category",
-        "GVWR Class",
-        "Fuel Type",
-        "Model Year",
-        "Fuel Technology",
-        "Electric Mile Range",
-        "Number of Vehicles Registered at the Same Address",
-        "Region",
-        "Vehicle Population",
-    ],
-}
-
-# Create a DataFrame
-df = pd.DataFrame(data)
-df.index = range(1, len(df) + 1)
-
-
-# Display the table in Streamlit
-st.markdown("---")
-st.title("Column Information")
-st.table(df)  # Use st.dataframe(df) if you want interactivity
 data = {
     "Column": [
         "Date",
@@ -76,6 +52,42 @@ data = {
 df = pd.DataFrame(data)
 df.index = range(1, len(df) + 1)
 
+
+# Display the table in Streamlit
+st.markdown("---")
+st.title("Column Information")
+st.table(df)  # Use st.dataframe(df) if you want interactivity
+data = {
+    "Variable": [
+        "Electric Mile Range",
+        "GVWR Class",
+        "Model Year",
+        "Fuel Type",
+        "Number of Vehicles Registered at the Same Address",
+        "Date",
+        "Vehicle Category",
+        "Fuel Technology",
+        "Region",
+        "Vehicle Population",
+    ],
+    "Missing Percentage (%)": [
+        97.413100,
+        55.937447,
+        1.468833,
+        0.209485,
+        0.080384,
+        0.000000,
+        0.000000,
+        0.000000,
+        0.000000,
+        0.000000,
+    ],
+}
+
+# Create a DataFrame
+df = pd.DataFrame(data)
+df.index = range(1, len(df) + 1)
+
 st.markdown("---")
 # Display the table in Streamlit
 st.title("Missing Data Percentage Overview")
@@ -92,7 +104,7 @@ st.markdown("""
 - **For rows where Model Year is missing, we fill the missing value with the median in train data.**
 - **Greatly enhanced the model's performance.**
 """)
-st.image("Vehicle_Age.png", width = 700)
+st.image("Vehicle_Age.png", width = 600)
 
 # Data for the table
 data = {
@@ -128,14 +140,20 @@ st.table(df)  #
 
 st.markdown("---")
 st.subheader("Model Performance & Interpretation")
-st.subheader("XGBoost Model : RMSE = 6043.28")
-st.subheader("Most Important Features: Vehicle Category, Fuel Type")
-st.image("feature_imp.png", width = 700)
+st.markdown("""
+### 
+- **XGBoost Model : RMSE = 6043.28**
+- **Most Important Features: Vehicle Category, Fuel Type**
+""")
+
+st.image("feature_imp.png", width = 600)
 
 st.markdown("---")
-col1, col2= st.columns(2)  # Three equal-width columns
-
-with col1:
-    st.write("On average, the model's predictions deviate by 6000 from the actual values, 2% deviation within the original variable's range of 1 to 300,000.")
-with col2:
-    st.write("New Varialbe (Vehicle Age) helps enhance model performance, while Vehicle Category plays most significant role in prediction.")
+st.subheader("Conclusion")
+st.markdown("""
+1. **On average, the model's predictions deviate by 6000 from the actual values, 2% deviation within the original variable's range of 1 to 300,000.**
+""")
+st.markdown("""
+2. **New Varialbe (Vehicle Age) helps enhance model performance, while Vehicle Category plays most significant role in prediction.**
+""")
+st.image("vehicle_category.png", width = 600)
